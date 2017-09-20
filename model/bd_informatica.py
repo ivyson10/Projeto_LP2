f6a6cb9efea89dcb06f571261966632b8689affd
ivyson10 <ivysonlucas10@gmail.com>
2017-09-20 16:15:25 -0300

    inicio da criação do banco de dados em sqlalquemy.gi

--- model/bd_informatica.py
+++ model/bd_informatica.py
@@ -1,5 +1,12 @@
-import sqlalchemy from create_engine
+from sqlalchemy import create_engine
+from sqlalchemy_utils import create_database, database_exists
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, MetaData, Float
 
+database  = "mysql+mysqldb://root:xrl832k.@localhost:3306/bdvendas.db"
 
-database  = ""
+engine = create_engine(database,echo = True)
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+metadata = MetaData(bind=engine)
 
