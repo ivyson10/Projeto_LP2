775764a3cac98463396f5e74b67bc7ea6d0fbc5f
ivyson10 <ivysonlucas10@gmail.com>
2017-10-03 11:03:21 +0000

    Atualizações de Design

--- view/login.py
+++ view/login.py
@@ -1,61 +1,8 @@
-from kivy.app import App
-from kivy.properties import StringProperty
-from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
-import os
+from kivy.lang import Builder 
+from kivy.uix.screenmanager import ScreenManager, Screen 
 
-from connected import Connected
-from regfunc import RootWidget
-from menu import RootWidget1
+
+Builder.load_file('login.kv') 
 
 class Login(Screen):
-    def do_login(self, loginText, passwordText):
-        app = App.get_running_app()
-
-        app.username = loginText
-        app.password = passwordText
-
-        self.manager.transition = SlideTransition(direction="left")
-        self.manager.current = 'menu'
-
-        app.config.read(app.get_application_config())
-        app.config.write()
-
-    def resetForm(self):
-        self.ids['login'].text = ""
-        self.ids['password'].text = ""
-
-    def do_registro(self):
-        
-        self.manager.transition = SlideTransition(direction="left")
-        self.manager.current = 'regfunc'
-
-class LoginApp(App):
-    username = StringProperty(None)
-    password = StringProperty(None)
-
-    def build(self):
-        manager = ScreenManager()
-        
-        manager.add_widget(Login(name='login'))
-        manager.add_widget(RootWidget(name='regfunc'))
-        manager.add_widget(RootWidget1(name='menu'))
-
-        return manager
-
-    def get_application_config(self):
-        if(not self.username):
-            return super(LoginApp, self).get_application_config()
-
-        conf_directory = self.user_data_dir + '/' + self.username
-
-        if(not os.path.exists(conf_directory)):
-            os.makedirs(conf_directory)
-
-        return super(LoginApp, self).get_application_config(
-            '%s/config.cfg' % (conf_directory)
-        )
-
-def main():
-    LoginApp().run()
-if __name__ == '__main__':
-    main()
+    pass
---
+++ view/principal.py
@@ -0,0 +1,10 @@
+from kivy.lang import Builder 
+from kivy.uix.screenmanager import ScreenManager, Screen 
+from kivymd.theming import ThemeManager
+
+
+
+Builder.load_file('principal.kv') 
+
+class Principal(Screen):
+    pass
