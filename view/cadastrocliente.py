fcdd3ae08fa98e50d679a6f6047851f3d6237ffc
ivyson10 <ivysonlucas10@gmail.com>
2017-10-03 18:53:36 -0300

    adicionado popup nas telas

--- view/cadastrocliente.py
+++ view/cadastrocliente.py
@@ -1,7 +1,25 @@
 from kivy.lang import Builder 
 from kivy.uix.screenmanager import ScreenManager, Screen 
+from kivymd.dialog import MDDialog
+from kivymd.label import MDLabel
+from kivy.metrics import dp
 
 Builder.load_file('cadastrocliente.kv') 
 
 class CadastroCliente(Screen):
-    pass
+    def aviso_cadastro(self):
+        content = MDLabel(font_style='Body1',
+                          theme_text_color='Secondary',
+                          text="Cliente Salvo Com Sucesso",
+                          size_hint_y=None,
+                          valign='top')
+        content.bind(texture_size=content.setter('size'))
+        self.dialog = MDDialog(title="Cliente!",
+                               content=content,
+                               size_hint=(.8, None),
+                               height=dp(200),
+                               auto_dismiss=False)
+
+        self.dialog.add_action_button("OK",
+                                      action=lambda *x: self.dialog.dismiss())
+        self.dialog.open()
--- view/cadastrovendedor.py
+++ view/cadastrovendedor.py
@@ -5,4 +5,19 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_file('cadastrovendedor.kv') 
 
 class CadastroVendedor(Screen):
-    pass
+    def aviso_cadastro(self):
+        content = MDLabel(font_style='Body1',
+                          theme_text_color='Secondary',
+                          text="Vendedor Salvo Com Sucesso",
+                          size_hint_y=None,
+                          valign='top')
+        content.bind(texture_size=content.setter('size'))
+        self.dialog = MDDialog(title="Vendedor!",
+                               content=content,
+                               size_hint=(.8, None),
+                               height=dp(200),
+                               auto_dismiss=False)
+
+        self.dialog.add_action_button("OK",
+                                      action=lambda *x: self.dialog.dismiss())
+        self.dialog.open()
